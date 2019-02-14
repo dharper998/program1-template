ccd3aa776734d5583d091b399b4cfe7b82a98d49
Alison <agarrit1@binghamton.edu>
2019-02-14 16:17:05 -0500

    commit from makefile

--- List.cpp
+++ List.cpp
@@ -11,7 +11,7 @@ Node::Node(Planet *p){
 List::List(){
 	this -> head = NULL;
 	this -> tail = NULL;
-	this -> size = 0;
+	this -> length = 0;
 }
 
 List::~List(){
@@ -24,7 +24,7 @@ List::~List(){
 void List::insert(int index, Planet *p){
 	Node *to_insert = new Node(p);
 	if(this == NULL){return;}
-	if(this -> size == 0){//if there's nothing
+	if(this -> length == 0){//if there's nothing
 		this -> head = to_insert;
 		this -> tail = to_insert;
 		this -> length++;
@@ -36,7 +36,7 @@ void List::insert(int index, Planet *p){
 		this -> length++;
 		return;
 	}
-	if(index >= this -> size){//if it wants to add at or past end
+	if(index >= this -> length){//if it wants to add at or past end
 		to_insert -> prev = this -> tail;
 		this -> tail = to_insert;
 		this -> length++;
@@ -55,4 +55,34 @@ void List::insert(int index, Planet *p){
 	to_insert -> prev = add_prev;
 	this -> length++;
 	return;
+}
+
+Planet *List::read(int index){
+	if(this == NULL || index >= this -> length){
+		return NULL;
+	}
+	node *temp = this -> head;
+	for(int i = 0; i < index; i++){
+		temp = temp -> next;
+	}
+	return temp -> data;
+}
+
+bool List::remove(int index){//NOT DONE
+	if(index >= this -> length){return false;}
+	if(index == 0){
+		node *to_delete = this -> head;
+		this -> head = this -> head -> next;
+		delete to_delete;
+		this -> length--;
+		return true;
+	}
+	if(index == this -> length - 1){
+		node *to_delete = this -> tail;
+		this -> tail = this -> tail -> prev;
+		delete to_delete;
+		this -> length--;
+		return true;
+	}
+	//TO ADD: DELETE IN MIDDLE
 }
